311b4c627df4ec5acca6ecce62a9a6d360ae2712
luss <lucia.soto@deimos-space.com>
2022-08-27 19:31:16 +0200

    L1C plot

--- l1c/src/l1c.py
+++ l1c/src/l1c.py
@@ -74,3 +74,31 @@ class l1c(initL1c):
         :return: NA
         '''
         #TODO
+
+    def plotL1cToa(self,lat_l1c,lon_l1c, toa_l1c, band):
+        '''
+        NOTE: doesn't work, bug in the color property
+        Plot the L1B and L1C grids superimposed
+        :param lat: L1B latitudes [deg]
+        :param lon: L1B longitudes [deg]
+        :param lat_l1c: L1C latitudes [deg] - MGRS grid
+        :param lon_l1c: L1C longitudes [deg]
+        :return: NA
+        '''
+
+        jet = cm.get_cmap('jet', len(lat_l1c))
+        toa_l1c[np.argwhere(toa_l1c<0)] = 0
+        max_toa = np.max(toa_l1c)
+
+        # Plot stuff
+        fig = plt.figure(figsize=(20,10))
+        for ii in range(100): # range(len(lat_l1c)):
+            clr = jet(toa_l1c[ii]/max_toa)
+            plt.plot(lon_l1c, lat_l1c, '.',color=clr, markersize=10)
+        plt.title('Projection on ground', fontsize=20)
+        plt.xlabel('Longitude [deg]', fontsize=16)
+        plt.ylabel('Latitude [deg]', fontsize=16)
+        plt.grid()
+        plt.axis('equal')
+        plt.savefig(self.outdir + 'toa_' + band + '.png')
+        plt.close(fig)
