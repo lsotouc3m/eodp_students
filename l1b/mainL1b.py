2e5f385120f207d221fcae982bf1c156f5c8dc92
luss <lucia.soto@deimos-space.com>
2022-10-27 20:13:27 +0200

    fixed main L1b

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -5,8 +5,8 @@ from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = '/home/luss/EODP/eodp/auxiliary'
-indir = '/home/luss/EODP/eodp/ism/test/ut02/output'
-outdir = '/home/luss/EODP/eodp/l1b/test/ut02/output'
+indir = '/home/luss/my_shared_folder/EODP_TER_2021/EODP-TS-L1B/input/'
+outdir = '/home/luss/my_shared_folder/l1b_out'
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
