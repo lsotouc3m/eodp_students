b98e59711d0c5896098eb8c2840c6801c3001729
Lucia Soto <lsoto@ing.uc3m.es>
2023-09-10 10:27:24 +0200

    Adapt paths to Windows syntax

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,9 +4,9 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = '/home/luss/EODP/eodp/auxiliary'
-indir = '/home/luss/my_shared_folder/sgm_out/gradient_alt100_act150/' # small scene
-outdir = '/home/luss/my_shared_folder/ism_out/'
+auxdir = r'C:\EODP\eodp_students\auxiliary'
+indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
+outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\myoutput"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
